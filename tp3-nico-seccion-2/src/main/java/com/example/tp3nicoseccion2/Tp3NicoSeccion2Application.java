2bfa87d39777fe41be69bf0dca7c5291428b9045
Zicophs <51864963+Zicophs@users.noreply.github.com>
2024-05-03 15:37:42 -0300

    seccion-2

---
+++ tp3-nico-seccion-2/src/main/java/com/example/tp3nicoseccion2/Tp3NicoSeccion2Application.java
@@ -0,0 +1,13 @@
+package com.example.tp3nicoseccion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3NicoSeccion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3NicoSeccion2Application.class, args);
+	}
+
+}
